851f0e84c5ad2a55b1be83102e3aec2668b3b2cf
Kaito <godlightdesu@gmail.com>
2024-02-25 03:57:30 +0800

    restruct class - skill

--- skill.py
+++ skill.py
@@ -2,51 +2,54 @@ import os
 
 from const import *
 
-class Skill:
-  
-  def __init__(self, name: str, color: str, used=False, texture=None) -> None:
-    self.name = name
+class FireWall():
+  def __init__(self, color: str, used=False, texture=None) -> None:
+    self.name = 'fw'
     self.color = color
     self.used = used
     self.texture = texture
     self.set_texture()
-    
+  
+  def __init__(self, color) -> None:
+    super().__init__('fw', color)
+  
   def set_texture(self):
     self.texture = os.path.join(
       f'assets/images/{self.color}_{self.name}.png')
 
-def use(board: object, player: object, which: str, target0: tuple, target1: tuple=None) -> bool:
+class Skill:
   
-  def useLB() -> bool:
+  def __init__(self) -> None:
+    self.skills = {
+      'lb' : self.useLB,
+      'fw' : self.useFW,
+      'vc' : self.useVC,
+      '404' :  self.use404
+    }
+  
+  def useLB(self) -> bool:
     return True
-  
-  def useFW() -> bool:
+    
+  def useFW(self) -> bool:
     return True
 
-  def useVC() -> bool:
+  def useVC(self) -> bool:
     # VirusChecker(color)
     return True
 
-  def use404() -> bool:
+  def use404(self) -> bool:
     # NotFound(color)
     return True
-  
-  skills = {
-    'lb' : useLB,
-    'fw' : useFW,
-    'vc' : useVC,
-    '404' :  use404
-  }
-  
-  # row, col = target0
-  # targetSq = board.squares[row][col]
-  
 
-  if which == '404' and target1 is None: return False
-  # elif len(target0) != 2:  return False
-  else: # valid target
-    used = skills[which]()
-    if used: 
-      player.skillLog.append(which)
-      print(player.skills[which]['log'])
-    return used
+  def use(self, board: object, player: object, which: str, target0: tuple, target1: tuple=None) -> bool:
+    # row, col = target0
+    # targetSq = board.squares[row][col]
+    
+    if which == '404' and target1 is None: return False
+    # elif len(target0) != 2:  return False
+    else: # valid target
+      used = self.skills[which]()
+      if used: 
+        player.skillLog.append(which)
+        print(player.skills[which]['log'])
+      return used
