1cad875668bad07008796b21cbda8edf1cdc8462
Kaito <godlightdesu@gmail.com>
2024-03-01 14:29:11 +0800

    impove thr AI (recursion)

---
+++ modules/utils.py
@@ -0,0 +1,130 @@
+import math
+from .const import *
+
+def Distance(game, startRow, startCol, targetRow, targetCol):
+  if game.view == 'blue': dR = BROW[targetRow] - startRow
+  else: dR = targetRow - startRow
+  dC = targetCol - startCol
+  
+  return int(math.sqrt(dR**2 + dC**2))
+
+def distanceToExit(game, color, startRow, startCol):
+    Exit1, Exit2 = EXITPOS[color]
+    row0, col0 = Exit1  # col 3
+    row1, col1 = Exit2  # col 4
+    # check Exit1
+    if startCol <= col0:
+      if game.view == 'blue': dR = BROW[row0] - startRow
+      else: dR = row0 - startRow
+      dC = col0 - startCol
+    # check Exit2
+    elif startCol >= col1:
+      if game.view == 'blue': dR = BROW[row1] - startRow
+      else: dR = row1 - startRow
+      dC = col1 - startCol
+    
+    return int(math.sqrt(dR**2 + dC**2))
+
+def scoreBoard(game):
+    '''
+    `Current player view`
+    '''
+    # it will be cheating that will know whether the piece is virus
+    # winner = game.checkGameOver()
+    # if winner is not None and winner == game.player.name: return 100
+    # elif winner is not None and winner != game.enemy.name: return -100
+    enemyPieces = len(game.board.getAllyPieces(game.enemy.color))
+    allyPieces = len(game.board.getAllyPieces(game.player.color))
+    if enemyPieces == 0: return 100
+    elif allyPieces == 0: return -100
+    
+    return scoreMaterial(game)
+
+def scoreMaterial(game):
+    '''
+    information can be collected
+    - self piece enter server
+    - self piece is captured?
+    - enemy piece enter server?
+    '''
+    score = 0
+    
+    allyLink, allyVirus = 0, 0
+    allyPieces = game.board.getAllyPieces(game.player.color)
+    enemyPieces = game.board.getAllyPieces(game.enemy.color)
+    # assume enemy will not enter virus
+    # enemyLink = 4 - game.player.link_eat -. round((game.enemy.link_enter + game.enemy.virus_enter) / 2)
+    # if enemyLink == 0 and game.player.link_eat == 4: return 1000
+    
+    if game.enemy.virus_eat == 4: return 1000
+    elif game.enemy.link_eat == 4: return -1000
+    
+    # pieces on board
+    for piece in allyPieces:
+      if piece.name == 'link': allyLink += 1
+      elif piece.name == 'virus': allyVirus += 1
+    score += allyLink * PIECEVALUE['link']
+    score += allyVirus * PIECEVALUE['virus']
+    
+    for piece in enemyPieces:
+      if piece.checked: score -= PIECEVALUE[piece.name]
+      else: score -= PIECEVALUE['unknown']
+    
+    # pieces ate
+    score -= PIECEVALUE['link'] * game.enemy.link_eat
+    score += PIECEVALUE['virus'] * game.enemy.virus_eat
+    
+    score += PIECEVALUE['unknown'] * (game.player.link_eat + game.player.virus_eat)
+    
+    # pieces in server
+    # print(game.enemy.name + ' serverStack:', end=' ')
+    for piece in game.enemy.serverStack:
+      # print(piece.name, end=' ')
+      if piece.name == 'link': score += PIECEVALUE['link'] * 2
+      elif piece.name == 'virus': score -= PIECEVALUE['virus'] * 3
+    # print()
+    
+    # print(game.player.name + ' serverStack:', end=' ')
+    for piece in game.player.serverStack:
+      # print(piece.name, end=' ')
+      if piece.checked and piece.name == 'virus': score += PIECEVALUE['virus'] * 3
+      # assume enemy will enter link
+      else: score -= PIECEVALUE['link'] * 2
+    # print()
+    
+    # bouns of link's dis to exit
+    for piece in allyPieces:
+      if piece.name == 'link': 
+        startRow, startCol = game.board.findPiecePos(piece)
+        if game.view == 'blue': startRow = BROW[startRow]
+        score += 10 - distanceToExit(game, game.player.color, startRow, startCol)
+    
+    for piece in enemyPieces:
+      startRow, startCol = game.board.findPiecePos(piece)
+      if game.view == 'blue': startRow = BROW[startRow]
+      score -= 10 - distanceToExit(game, game.enemy.color, startRow, startCol)
+    
+    # bouns of virus's dis to enemy pieces
+    for piece in allyPieces:
+      dMin = 7
+      # if piece.name == 'virus':
+      startRow, startCol = game.board.findPiecePos(piece)
+      for ePiece in enemyPieces:
+        endRow, endCol = game.board.findPiecePos(ePiece)
+        dst = Distance(game, startRow, startCol, endRow, endCol)
+        if dst < dMin: dst = dMin
+      score += 10 - dMin
+    
+    for piece in enemyPieces:
+      dMin = 7
+      # if piece.name == 'virus':
+      startRow, startCol = game.board.findPiecePos(piece)
+      for ePiece in allyPieces:
+        endRow, endCol = game.board.findPiecePos(ePiece)
+        dst = Distance(game, startRow, startCol, endRow, endCol)
+        if dst < dMin: dst = dMin
+      score -= 10 - dMin
+    
+    # print(game.player.name + '\'s score :', score)
+    return score
+  
